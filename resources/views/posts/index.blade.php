d8b16e7e7e188096c43007a4c954e3b70d346bed
HossamSalaheldeen <hossam.salah1995@gmail.com>
2021-03-21 19:36:13 +0200

    version2.3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,13 +29,32 @@
          
           <x-button type="info" href="{{ route('posts.show',['post' => $post->id]) }}">View</x-button>
           <x-button type="secondary" href="{{ route('posts.edit',['post' => $post->id]) }}">Edit</x-button>
-           <form style="display:inline !important" method="POST"  action="{{route('posts.destroy', ['post' => $post->id])}}">
-            @csrf
-            @method('DELETE')
-            <button style="display:inline !important; margin-top:-20px !important" class="btn btn-danger" type="submit">Delete</button>
-          </form>
-        </td>
-      </tr>
+          <button style="display:inline !important; margin-top:-20px !important" class="btn btn-danger" data-toggle="modal" data-target="#del_post_{{$post->id}}">Delete</button>
+           </td>
+          </tr>
+           <div id="del_post_{{$post->id}}" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+            <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title">Delete</h4>
+                    <button type="button" class="close" data-dismiss="modal">×</button>
+                </div>
+                <form style="display:inline !important" method="POST"  action="{{route('posts.destroy', ['post' => $post->id])}}">
+                  @csrf
+                  @method('DELETE')
+                  <div class="modal-body">
+                      <h4>Do you want to delete post with title {{$post->title}} ?</h4>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-info" data-dismiss="modal">No</button>
+                      <input class="btn btn-danger" type="submit" value="Yes">
+                    </div>
+                </form>
+          </div>
+        </div>
+      </div>
+        
     @endforeach
     </tbody>
 </table>
